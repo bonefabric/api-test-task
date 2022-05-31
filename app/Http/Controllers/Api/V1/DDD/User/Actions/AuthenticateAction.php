1a8bd0343f6c9174edeed55fcc9906ee21433a31
BoneFabric <iburanguloff@ya.ru>
2022-05-31 08:10:26 +0500

    actions

---
+++ app/Http/Controllers/Api/V1/DDD/User/Actions/AuthenticateAction.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Api\V1\DDD\User\Actions;
+
+use App\Http\Controllers\Api\ApiController;
+
+class AuthenticateAction extends ApiController
+{
+    //
+}
